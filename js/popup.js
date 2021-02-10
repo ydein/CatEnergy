57a40662a888dc5f9f049b3b46fa9a4100919dd2
ydein <46528774+ydein@users.noreply.github.com>
2021-02-10 15:25:00 +0200

    Add files via upload

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,10 @@
 
+	jQuery(function($){
+	   $("#date").mask("99/99/9999");
+	   $("#phone").mask("(999) 999-9999");
+	   $("#tin").mask("99-9999999");
+	   $("#ssn").mask("999-99-9999");
+	});
 
 	var navigationLink = document.querySelector(".navigation__toggle--open");
 	var navigationPopup = document.querySelector(".navigation-header");
